28d937d4f3132755fe235e91dff61d96850e3714
CacioDavi <davicacio4@gmail.com>
2025-11-20 19:04:36 -0300

    style: ajusts on formatation and colors
    added type colors on card backgrounds and some adjusts on formatation problems.

---
+++ assets/js/poke-api.js
@@ -0,0 +1,35 @@
+const pokeApi = {};
+
+function applyApiOnModel (pokeDetail) {
+
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.order;
+    pokemon.name = pokeDetail.name;
+
+    const types = pokeDetail.types.map((typeSlots) => typeSlots.type.name);
+    const [type] = types;
+
+    pokemon.type = type;
+    pokemon.types = types;
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+    return pokemon;
+
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then(((details) => details.json()))
+        .then(applyApiOnModel)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails) 
+} 
